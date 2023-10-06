3f8f80fce6648a7d18bc86c4e624df5a79621589
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-06 09:52:48 -0300

    rodata 14AA0

---
+++ src/main/3440.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002840.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_8000291C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_800029A8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002AF4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002BE8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002C50.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002CB8.s")
