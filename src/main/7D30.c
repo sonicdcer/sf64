54074bae93c8309ffd054613c0cceb0a23de047b
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-06 10:39:19 -0300

    func_80007130

--- src/main/7D30.c
+++ src/main/7D30.c
@@ -1,6 +1,16 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7D30/func_80007130.s")
+s32 func_800234B0(s32*, u8, s32, s32);
+
+extern s32 D_800E2128;
+
+s32 func_80007130(s32 arg0, s32 arg1) {
+    if (func_800234B0(&D_800E2128, arg0, arg1, arg0) != 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/7D30/func_8000716C.s")
 
