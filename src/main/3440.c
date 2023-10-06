7e6c70f4f4b8d41ed61df4150efebdd5c8e30b7c
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-06 10:48:51 -0300

    func_80002C50

--- src/main/3440.c
+++ src/main/3440.c
@@ -10,6 +10,20 @@
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002BE8.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002C50.s")
+s32 func_800071FC(s32*);
+s32 func_80020720(s32*, s32, s32);
+
+extern s32 D_800E2318;
+extern u8 D_80137E80;
+extern s32 D_80144F60;
+
+void func_80002C50(void) {
+    if ((D_80137E80 == 0) && (func_800071FC(&D_80144F60) == 0)) {
+        func_80020720(&D_800E2318, 15, 0);
+        return;
+    }
+    func_80020720(&D_800E2318, 14, 0);
+}
+
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002CB8.s")
