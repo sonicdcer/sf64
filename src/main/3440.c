89eadaeeadc4aaf3d08468b581206b11e8971265
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-06 10:58:53 -0300

    func_80002BE8

--- src/main/3440.c
+++ src/main/3440.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+s32 func_800071FC(s32*);
+s32 func_80020720(s32*, s32, s32);
+
+extern s32 D_800E2318;
+extern u8 D_80137E80;
+extern s32 D_80144F60;
+
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002840.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_8000291C.s")
@@ -8,14 +15,13 @@
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002AF4.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/3440/func_80002BE8.s")
-
-s32 func_800071FC(s32*);
-s32 func_80020720(s32*, s32, s32);
-
-extern s32 D_800E2318;
-extern u8 D_80137E80;
-extern s32 D_80144F60;
+void func_80002BE8(void) {
+    if ((D_80137E80 == 0) && (func_800072E0(&D_80144F60) == 0)) {
+        func_80020720(&D_800E2318, 15, 0);
+        return;
+    }
+    func_80020720(&D_800E2318, 14, 0);
+}
 
 void func_80002C50(void) {
     if ((D_80137E80 == 0) && (func_800071FC(&D_80144F60) == 0)) {
