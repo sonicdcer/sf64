d020e5519f16bb8d8501945ae5adc06dc8c86499
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-06 11:11:48 -0300

    rodata

---
+++ src/main/7FC0.c
@@ -0,0 +1,73 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_800073C0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_800074AC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_80007604.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_8000762C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_8000770C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8240.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C824C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8260.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8278.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8290.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C82A8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C82C0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C82D4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C82E8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8300.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8318.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8330.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8348.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C835C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C836C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8388.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C83A4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C83BC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C83D8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C83F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8404.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8418.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8424.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/D_800C8430.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_800077F8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_80007880.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_80007910.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_80007CEC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_80007D58.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_80007FE4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7FC0/func_80008018.s")
---
+++ src/main/8CC0.c
@@ -0,0 +1,41 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_800080C0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_80008364.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_80008780.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_80009124.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_80009504.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_8000967C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_800097A8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_800098DC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_80009984.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_80009A2C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_80009AAC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_80009B64.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_80009D78.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_8000A128.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_8000A25C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_8000A700.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_8000B3F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_8000B480.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_8000B51C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/8CC0/func_8000B98C.s")
