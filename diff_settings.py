a90ed2a99092c0c169657f2246859e073f91b370
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-06 09:38:01 -0300

    diff settings

---
+++ diff_settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def apply(config, args):
+    config['baseimg'] = 'baserom.us.z64'
+    config['myimg'] = 'build/starfox64.us.z64'
+    config['mapfile'] = 'build/starfox64.us.map'
+    config['source_directories'] = ['./src','./include']
+    config['objdump_flags'] = ['-M','reg-names=32']
+    config['makeflags'] = ["KEEP_MDEBUG=1"]
