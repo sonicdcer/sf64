646e6f983be12cc9b4b9a37c295cbd27cab9fd43
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-07 08:43:15 -0300

    func_80004D00

--- src/main/4650.c
+++ src/main/4650.c
@@ -1,13 +1,22 @@
 #include "common.h"
 
-extern s32 D_80138E90;
+extern s32 D_800E2010;
+extern s32 D_800E2028;
 extern s32 D_8013A040;
-extern s32 func_80004D00;
+extern s32 D_8013B1F0;
+extern s32 D_80138E90;
 
+void func_800034E8(s32);
 s32 func_80003A50(void);
+void func_80003EE0(void);
+void func_80004ABC(s32);
+void func_80008018(void);
+void func_800221A0(s32*, s32, void*, s32, s32*, s32);
+void func_800222F0(s32*);
+void func_80022440(s32);
+void func_800227A0(s32, s32*, s32*, s32);
+void func_80022A80(s32, s32);
 s32 func_8001EF10(void);
-s32 func_800221A0(s32*, s32, s32*, s32, s32*, s32);
-s32 func_800222F0(s32*);
 s32 func_80022B60(void);
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/4650/func_80003A50.s")
@@ -38,7 +47,18 @@ s32 func_80022B60(void);
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/4650/func_80004ABC.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/4650/func_80004D00.s")
+void func_80004D00(s32 arg0) {
+    func_80022440(0xFE);
+    func_80003EE0();
+    func_800034E8(1);
+    func_800227A0(0x96, &D_800E2010, &D_800E2028, 0x32);
+    func_800221A0(&D_8013A040, 3, &func_80004ABC, arg0, &D_8013B1F0, 0x64);
+    func_800222F0(&D_8013A040);
+    func_80008018();
+    func_80022A80(0, 0);
+loop_1:
+    goto loop_1;
+}
 
 void bootproc(void) {
     func_8001EF10();
