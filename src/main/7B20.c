df3ba381adfe74365eecb1cddac32dc6897feda9
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-07 05:35:32 -0300

    defake func_80006F38

--- src/main/7B20.c
+++ src/main/7B20.c
@@ -7,17 +7,10 @@ void func_80006F20(void) {
     D_80144BD0 = &D_8013C3D0;
 }
 
-void func_80006F38(s32 arg0) {
-    //! FAKE:
-    s32 new_var;
-    s32 new_var2;
-    s32* new_var3;
-
-    if (!D_80144BD0) {}
-
-    new_var2 = arg0;
-    new_var = *(new_var3 = &new_var2);
-    D_80144BD0 = D_80144BD0 + ((new_var + 0xF) & (~0xF));
+s32 func_80006F38(s32 arg0) {
+    s32 tmp = D_80144BD0;
+    D_80144BD0 = ((arg0 + 0xF) & ~0xF) + D_80144BD0;
+    return tmp;
 }
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/7B20/func_80006F60.s")
