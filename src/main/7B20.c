26a82c33edb5161b57be6210173a16a1818e370b
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-07 06:09:40 -0300

    func_80007068

--- src/main/7B20.c
+++ src/main/7B20.c
@@ -9,16 +9,19 @@ void func_80006F20(void) {
 
 s32 func_80006F38(s32 arg0) {
     s32 tmp = D_80144BD0;
-    
+
     D_80144BD0 = ((arg0 + 0xF) & ~0xF) + D_80144BD0;
     return tmp;
 }
 
+// https://decomp.me/scratch/uf9Ap 98.50%
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/7B20/func_80006F60.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/7B20/func_80006FD8.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/7B20/func_80007068.s")
+void func_80007068(s32* arg0, s32 arg1) {
+    *arg0 += arg1;
+}
 
 void func_8000707C(s32* arg0, s32 arg1) {
     *arg0 = arg1;
