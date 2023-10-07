cf5c411be812f8ffc699413ece600ebeba8d6331
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-07 03:29:38 -0300

    bootproc

--- src/main/4650.c
+++ src/main/4650.c
@@ -1,5 +1,15 @@
 #include "common.h"
 
+extern s32 D_80138E90;
+extern s32 D_8013A040;
+extern s32 func_80004D00;
+
+s32 func_80003A50(void);
+s32 func_8001EF10(void);
+s32 func_800221A0(s32*, s32, s32*, s32, s32*, s32);
+s32 func_800222F0(s32*);
+s32 func_80022B60(void);
+
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/4650/func_80003A50.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/4650/func_80003B48.s")
@@ -30,4 +40,10 @@
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/4650/func_80004D00.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/4650/bootproc.s")
+void bootproc(void) {
+    func_8001EF10();
+    func_80022B60();
+    func_80003A50();
+    func_800221A0(&D_80138E90, 1, &func_80004D00, 0, &D_8013A040, 0xFF);
+    func_800222F0(&D_80138E90);
+}
