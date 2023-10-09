fbed966fbdb70ac05c9fb73eedc6bcb3a1fa1fd4
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-09 04:51:56 -0300

    20A60 to c

---
+++ src/main/20A60.c
@@ -0,0 +1,47 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FE60.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FE6C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FE78.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FE88.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FE98.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FEA4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FEB0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FEC0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FED0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FEDC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FEE8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FEF8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FF08.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FF14.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FF20.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FF30.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FF40.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FF5C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FF7C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FF9C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_8001FFC0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_800201B8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/20A60/func_80020288.s")
