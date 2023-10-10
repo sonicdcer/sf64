a8d3238531010150d1ab36f8c1b7d8c8765eaac8
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 04:54:50 -0300

    func_8002C6B8

--- src/main/2C700.c
+++ src/main/2C700.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+s32 func_8002DE78(s32, s32, s32);
+
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/2C700/func_8002BB00.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/2C700/func_8002BB7C.s")
@@ -22,4 +24,32 @@
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/2C700/func_8002C698.s")
 
+
+typedef struct {
+    char pad_0[0x4];
+    u8 unk4;
+} Unkstruct_func_8002C6B8_1;
+
+typedef struct {
+    char pad_0[0x4];
+    s8 unk4;
+    char pad_5;
+    s16 unk6;
+    char pad_[0x4];
+    s32 unk12;
+} Unkstruct_func_8002C6B8_2;
+
+// matches with -O1 -g0 IDO 5.3/7.1
+#ifdef NON_MATCHING
+s32 func_8002C6B8(Unkstruct_func_8002C6B8_1* arg0) {
+    Unkstruct_func_8002C6B8_2 temp;
+
+    temp.unk4 = arg0->unk4;
+    temp.unk12 = 0;
+    temp.unk6 = 0;
+    func_8002DE78(&temp, 16, 1);
+    return 0;
+}
+#else
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/2C700/func_8002C6B8.s")
+#endif
