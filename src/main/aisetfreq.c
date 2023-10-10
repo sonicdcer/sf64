754051e0a1648c1f20e59330a6cad7c3566bca99
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 02:26:58 -0300

    settime aisetfreq

---
+++ src/main/aisetfreq.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/aisetfreq/func_80024D60.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/aisetfreq/func_80024EC0.s")
---
+++ src/main/settime.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/settime/func_80024D20.s")
