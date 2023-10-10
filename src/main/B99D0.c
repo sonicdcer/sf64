eb5df980c1a69d64bef41a30fbd8681f1b15f960
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 04:57:29 -0300

    0xB99D0 to C

---
+++ src/main/B99D0.c
@@ -0,0 +1,109 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B8DD0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B8E14.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B8F18.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B8F48.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B8F78.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B8FA8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B8FD8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9008.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9120.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9150.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9180.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9298.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B92C8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B92F8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9328.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9358.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9388.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B94A0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B94D0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B95E8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9618.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9730.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9760.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9790.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B98A8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B99C0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B99F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9A20.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9B38.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9C50.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9D68.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9E80.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9EB0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800B9FC8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA0E0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA110.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA140.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA170.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA1A0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA1D0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA2E8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA400.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA430.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA460.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA490.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA4C0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA4F0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA520.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA550.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA580.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA5B0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA5E0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA610.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/B99D0/func_800BA640.s")
