48adf3541768335410fb8cf572e934c8553adcb0
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 09:04:32 -0300

    2E9E0

---
+++ src/libultra/2E9E0.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/2E9E0/func_8002DDE0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/2E9E0/func_8002DE78.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/2E9E0/func_8002DF20.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/2E9E0/func_8002E040.s")
