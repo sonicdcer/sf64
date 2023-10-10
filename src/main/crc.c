3d07b60ac9adec50a9580a1466e09bcd726de577
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 04:00:39 -0300

    crc

--- src/main/crc.c
+++ src/main/crc.c
@@ -3,11 +3,3 @@
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/crc/func_80026440.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/crc/func_800264F0.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/crc/func_800265C0.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/crc/func_800267C4.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/crc/func_80026940.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/crc/func_80026B5C.s")
