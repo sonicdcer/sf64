2f9d51b34c505bc577b4c4c36c041fe3ab9f0b7f
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 03:57:33 -0300

    ll

---
+++ src/main/ll.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023C90.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023CBC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023CF8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023D34.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023D60.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023D9C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023DF8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023E28.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023E88.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/ll/func_80023F24.s")
