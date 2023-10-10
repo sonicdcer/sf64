c6278e068da5ef1eb3104a0cade0227946d92dcb
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 03:50:42 -0300

    sptask

---
+++ src/main/sptask.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/sptask/func_80021EA0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/sptask/func_80021FBC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/sptask/func_8002214C.s")
--- src/main/sptaskyield.c
+++ src/main/sptaskyield.c
@@ -1,9 +1,3 @@
 #include "common.h"
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/sptaskyield/func_80021E80.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/sptaskyield/func_80021EA0.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/sptaskyield/func_80021FBC.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/sptaskyield/func_8002214C.s")
