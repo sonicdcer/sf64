028b0d855ca2c84e650993ed1e7f3364cccdf7f5
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 03:42:56 -0300

    setglobalintmask, epirawread

--- src/main/epirawread.c
+++ src/main/epirawread.c
@@ -1,5 +1,3 @@
 #include "common.h"
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/epirawread/func_8002AEC0.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/epirawread/func_8002AF10.s")
---
+++ src/main/setglobalintmask.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/setglobalintmask/func_8002AF10.s")
