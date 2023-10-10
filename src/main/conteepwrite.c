cc11cd2c7591d67e7e30088069ff099effcc22ce
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 03:56:30 -0300

    conteepwrite

--- src/main/conteepwrite.c
+++ src/main/conteepwrite.c
@@ -5,25 +5,3 @@
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023960.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023A6C.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023C90.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023CBC.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023CF8.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023D34.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023D60.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023D9C.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023DF8.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023E28.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023E88.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023F24.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/conteepwrite/func_80023F50.s")
