67cba44e5b5ddb15016faeb06cd3761b6052b43f
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 02:10:16 -0300

    libultra identified segments

---
+++ src/main/contreaddata.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/contreaddata/func_80020380.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/contreaddata/func_80020444.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/contreaddata/func_800204EC.s")
---
+++ src/main/recvmesg.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/recvmesg/func_800205E0.s")
