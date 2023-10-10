4efc9e32d35895831e90fdf37e9eb61f6d51e375
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-10 02:23:14 -0300

    string

---
+++ src/main/string.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/string/func_80023FC0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/string/func_80023FEC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/string/func_80024014.s")
