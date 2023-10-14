bd77fc3cdfacaa69bd1aaa43a40984104033bae1
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-14 07:02:31 -0300

    format code

--- src/libultra/io/pidma.c
+++ src/libultra/io/pidma.c
@@ -21,20 +21,20 @@ s32 osPiStartDma(test* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s
     if (__osPiDevMgr == 0) {
         return -1;
     }
-    
+
     if (arg2 == 0) {
         arg0->unk0 = 0xB;
     } else {
         arg0->unk0 = 0xC;
     }
-    
-    arg0->unk2 = (s8) arg1;
+
+    arg0->unk2 = (s8)arg1;
     arg0->unk4 = arg6;
     arg0->unk8 = arg4;
     arg0->unkC = arg3;
     arg0->unk10 = arg5;
     arg0->unk14 = 0;
-    
+
     if (arg1 == 1) {
         result = osJamMesg(osPiGetCmdQueue(), arg0, 0);
     } else {
