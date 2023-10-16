8436688fb6e56697163b2870a17b2f419ea6b2ba
petrie911 <69443847+petrie911@users.noreply.github.com>
2023-10-16 15:58:31 -0500

    better organization (#3)
    Co-authored-by: petrie911 <petrie911@Monday.localdomain>

--- src/overlay/E51970.c
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/E51970/func_80187520_E51970.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/E51970/D_8019F6E0_E69B30.s")
---
+++ src/overlay/segment_DF4260/DF4260.c
@@ -0,0 +1,131 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80187520_DF4260.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80187530_DF4270.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018753C_DF427C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_801875A4_DF42E4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80187670_DF43B0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80187710_DF4450.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_801877A0_DF44E0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80187838_DF4578.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_801878D8_DF4618.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018798C_DF46CC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_801879F0_DF4730.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80187A38_DF4778.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80187A88_DF47C8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80187AC8_DF4808.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80188750_DF5490.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_801887AC_DF54EC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80188A18_DF5758.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80188C7C_DF59BC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80188D50_DF5A90.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80189058_DF5D98.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018A434_DF7174.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018A730_DF7470.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018AA74_DF77B4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018AB08_DF7848.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018AB64_DF78A4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018ACE0_DF7A20.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018AED0_DF7C10.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018B0B4_DF7DF4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018B15C_DF7E9C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018B418_DF8158.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018B58C_DF82CC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018BAAC_DF87EC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018BAFC_DF883C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018BBA4_DF88E4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018BBF8_DF8938.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018BC50_DF8990.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018BC84_DF89C4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018BD7C_DF8ABC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018BDD4_DF8B14.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018BE7C_DF8BBC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018C0B0_DF8DF0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018C19C_DF8EDC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018DDAC_DFAAEC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018E290_DFAFD0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018E76C_DFB4AC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018EC54_DFB994.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018ECAC_DFB9EC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018ED1C_DFBA5C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018ED78_DFBAB8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018EE2C_DFBB6C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018EE84_DFBBC4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018EF38_DFBC78.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018EF90_DFBCD0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018F044_DFBD84.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018F1C8_DFBF08.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018F31C_DFC05C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018F3BC_DFC0FC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018F4A4_DFC1E4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018F55C_DFC29C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018F678_DFC3B8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018F6F8_DFC438.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_8018F880_DFC5C0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80190F74_DFDCB4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/func_80191160_DFDEA0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlay/segment_DF4260/DF4260/D_8019AEF0_E07C30.s")
