742a17763783e156ed72181b28be4d0cba3a05b2
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-16 13:32:30 -0300

    // Joybus commands

--- include/PR/controller.h
+++ include/PR/controller.h
@@ -4,8 +4,22 @@
 #include "global.h"
 #include "PR/os.h"
 
-#define UNK(n) 500000 * (u64)(n) / 1000000
+// Joybus commands
 #define CONT_CMD_REQUEST_STATUS 0
+#define CONT_CMD_READ_BUTTON    1
+#define CONT_CMD_READ_PAK       2
+#define CONT_CMD_WRITE_PAK      3
+#define CONT_CMD_READ_EEPROM    4
+#define CONT_CMD_WRITE_EEPROM   5
+#define CONT_CMD_READ36_VOICE   9
+#define CONT_CMD_WRITE20_VOICE  10
+#define CONT_CMD_READ2_VOICE    11
+#define CONT_CMD_WRITE4_VOICE   12
+#define CONT_CMD_SWRITE_VOICE   13
+#define CONT_CMD_CHANNEL_RESET  0xFD
+#define CONT_CMD_RESET          0xFF
+
+#define UNK(n) 500000 * (u64)(n) / 1000000
 
 typedef struct
 {
