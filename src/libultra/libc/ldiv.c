a36b61f73bc2839ce963cd8ebaa85d5195414330
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-16 13:59:04 -0300

    decompile ldiv

--- src/libultra/libc/ldiv.c
+++ src/libultra/libc/ldiv.c
@@ -1,5 +1,17 @@
-#include "common.h"
+#include "global.h"
+#include "libc/stdlib.h"
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/libc/ldiv/lldiv.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/libc/ldiv/ldiv.s")
+ldiv_t ldiv(long numer, long denom) {
+    ldiv_t val;
+
+    val.quot = numer / denom;
+    val.rem = numer - denom * val.quot;
+    if ((val.quot < 0) && (val.rem > 0)) {
+        val.quot++;
+        val.rem -= denom;
+    }
+
+    return val;
+}
