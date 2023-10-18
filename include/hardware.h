5d1b103e75e24fa44f0f0307028be5c53ee2d61b
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-18 10:42:28 -0300

    add AI_LEN_REG

--- include/hardware.h
+++ include/hardware.h
@@ -4,6 +4,7 @@
 #define HW_REG(reg, type) *(volatile type *)(uintptr_t)((reg) | 0xa0000000)
 
 #define AI_STATUS_REG 0x0450000C
+#define AI_LEN_REG 0x04500004
 #define AI_STATUS_AI_FULL (1 << 31)
 
 #endif
