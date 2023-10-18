4e1b602d1e9040c8c71b566dd44734e676f41aaf
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-18 10:27:20 -0300

    decomp __osAiDeviceBusy

--- src/libultra/io/ai.c
+++ src/libultra/io/ai.c
@@ -1,3 +1,19 @@
-#include "common.h"
+#include "global.h"
+#include "PR/rcp.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/io/ai/__osAiDeviceBusy.s")
+// TODO: Put in proper header:
+typedef unsigned long int uintptr_t;
+
+#define HW_REG(reg, type) *(volatile type*)(uintptr_t)((reg) | 0xa0000000)
+#define AI_STATUS_REG 0x0450000C
+#define AI_STATUS_AI_FULL (1 << 31)
+
+s32 __osAiDeviceBusy(void) {
+    register s32 status = HW_REG(AI_STATUS_REG, u32);
+
+    if (status & AI_STATUS_AI_FULL) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
