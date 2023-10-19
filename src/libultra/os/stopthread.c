44a1d0bb06ad912326d32b8687f752b5194797b6
petrie911 <petrie911@yahoo.com>
2023-10-19 09:20:44 -0500

    let's all go to the library

---
+++ src/libultra/os/stopthread.c
@@ -0,0 +1,35 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/osStopThread.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E340.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E364.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E3A8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E3BC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E3E0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E4F8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E548.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E5E0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E604.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E628.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E64C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E670.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E694.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E6B8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/func_8002E6DC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/os/stopthread/D_800C9A50.s")
---
+++ src/main/2DF00.c
@@ -0,0 +1,29 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002D300.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002D3AC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002D494.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002D654.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002D660.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002D7BC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002D9E4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002DA78.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002DB3C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002DB7C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002DC38.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002DC68.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002DCD0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/main/2DF00/func_8002DD00.s")
