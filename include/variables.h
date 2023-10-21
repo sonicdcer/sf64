7f5aa3ffbc94e768b00d295a0294edb8c80d25a2
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-10-21 14:15:14 -0300

    declare segmented addresses in func_80088784

--- include/variables.h
+++ include/variables.h
@@ -26,6 +26,37 @@ extern Gfx D_1024230[];
 extern Gfx D_1024830[];
 extern Gfx D_102A8A0[];
 extern Gfx D_1024AC0[];
+
+
+extern Gfx D_2000000[];
+extern Gfx D_2000890[];
+extern Gfx D_2001090[];
+extern Gfx D_2001120[];
+extern Gfx D_20019B0[];
+extern Gfx D_2002120[];
+extern Gfx D_2002240[];
+extern Gfx D_2002AD0[];
+extern Gfx D_2003360[];
+extern Gfx D_2003BF0[];
+extern Gfx D_2004240[];
+extern Gfx D_2004480[];
+extern Gfx D_2004D10[];
+extern Gfx D_20052D0[];
+extern Gfx D_20055A0[];
+extern Gfx D_2005E30[];
+extern Gfx D_2006360[];
+extern Gfx D_20066C0[];
+extern Gfx D_20073F0[];
+extern Gfx D_2008480[];
+extern Gfx D_2009510[];
+extern Gfx D_200A5A0[];
+extern Gfx D_200B630[];
+extern Gfx D_200C6C0[];
+extern Gfx D_200E7F0[];
+extern Gfx D_200F080[];
+extern Gfx D_200F910[];
+extern Gfx D_20101A0[];
+
 extern Gfx D_6004570[];
 extern Gfx D_D0098B0[];
 extern Gfx D_F014180[];
