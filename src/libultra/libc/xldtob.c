fc3b8d520c17407fb53707781611cb9b7d100bd2
petrie911 <petrie911@yahoo.com>
2023-11-02 14:07:32 -0500

    missed some things

---
+++ src/libultra/libc/xldtob.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/libc/xldtob/func_80029080.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/libc/xldtob/D_800C9580.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/libc/xldtob/_Ldtob.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libultra/libc/xldtob/D_800C95D0.s")
