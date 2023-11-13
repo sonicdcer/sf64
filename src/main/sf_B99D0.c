00af14023a1c7b0ed5f1f843ec8d0158531cf21b
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-11-13 06:19:29 -0300

    func_800B99C0

--- src/main/sf_B99D0.c
+++ src/main/sf_B99D0.c
@@ -127,7 +127,9 @@ void func_800B98A8(s32 r, s32 g, s32 b, s32 a, s32 nr, s32 fr) {
     gSPFogFactor(gMasterDisp++, (0x1F400 / (fr - nr)), (((0x1F4 - nr) * 256) / (fr - nr)));
 }
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/sf_B99D0/func_800B99C0.s")
+void func_800B99C0(void) {
+    gSPDisplayList(gMasterDisp++, D_800D3CF0);
+}
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/sf_B99D0/func_800B99F0.s")
 
