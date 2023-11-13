2947916a4c6d216ef72d162193e36cbe612a21b3
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-11-13 08:43:39 -0300

    func_800843FC

--- src/main/sf_84F70.c
+++ src/main/sf_84F70.c
@@ -1,8 +1,11 @@
-#include "common.h"
+#include "global.h"
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/sf_84F70/func_80084370.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/sf_84F70/func_800843FC.s")
+void func_800843FC(s32 arg0) {
+    func_8009F574(&gMasterDisp, 0, 0, SCREEN_WIDTH, arg0, 0, 0, 0, 255);
+    func_8009F574(&gMasterDisp, 0, SCREEN_HEIGHT - arg0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0, 255);
+}
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/sf_84F70/func_80084488.s")
 
