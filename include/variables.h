9729148cfe083033b5a2b3779d957a479323e4f4
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-11-13 06:23:02 -0300

    func_800B9EB0

--- include/variables.h
+++ include/variables.h
@@ -100,6 +100,7 @@ extern Gfx D_800D3DC8[];
 extern Gfx D_800D3EE8[];
 extern Gfx D_800D4050[];
 extern Gfx D_800D4128[];
+extern Gfx D_800D41B8[];
 extern Gfx D_800D4290[];
 extern Gfx D_800D4320[];
 extern Gfx D_800D4440[];
