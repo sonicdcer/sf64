660a12a2c02c1ce7ee7df17eb20e8c502d51eb15
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-12-18 17:07:21 -0300

    func_EBFBE0_80187520

--- src/overlays/ovl_EBFBE0/EBFBE0.c
+++ src/overlays/ovl_EBFBE0/EBFBE0.c
@@ -1,3 +1,48 @@
-#include "common.h"
+#include "global.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_EBFBE0/EBFBE0/func_EBFBE0_80187520.s")
+void func_EBFBE0_80187754(void);
+void func_EBFBE0_801878D8(void);
+void func_EBFBE0_801920C4(void);
+void func_EBFBE0_80192340(void);
+void func_EBFBE0_8019E85C(void);
+void func_EBFBE0_8019E8C8(void);
+void func_EBFBE0_8019E8D0(void);
+void func_EBFBE0_801A01A8(void);
+
+void func_EBFBE0_80187520(s32 arg0, s32 arg1) {
+    switch (arg0) {
+        case 0x67:
+            func_EBFBE0_80187754();
+            break;
+
+        case 0x68:
+            func_EBFBE0_801878D8();
+            break;
+
+        case 0x69:
+            func_EBFBE0_8019E8D0();
+            break;
+
+        case 0x6A:
+            func_EBFBE0_801A01A8();
+            break;
+
+        case 0x6B:
+            func_EBFBE0_801920C4();
+            break;
+
+        case 0x6C:
+            func_EBFBE0_80192340();
+            break;
+
+        case 0x6D:
+            func_EBFBE0_8019E85C();
+            break;
+
+        case 0x6E:
+            func_EBFBE0_8019E8C8();
+
+        default:
+            break;
+    }
+}
