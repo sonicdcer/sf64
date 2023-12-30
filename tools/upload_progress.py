67601602edefb5fe58ed2771a9cf5a8058d43504
KiritoDv <kiritodev01@gmail.com>
2023-12-30 14:29:43 -0600

    Discord matching progress inside CI

---
+++ tools/upload_progress.py
@@ -0,0 +1,32 @@
+from progress import *
+import requests
+import argparse
+
+def main(args):
+    func_sizes, total_size = get_func_sizes()
+    all_funcs = set(func_sizes.keys())
+
+    nonmatching_funcs = get_nonmatching_funcs()
+    matching_funcs = all_funcs - nonmatching_funcs
+
+    matching_size, nonmatching_size = get_funcs_sizes(func_sizes, matching_funcs, nonmatching_funcs)
+
+    if len(all_funcs) == 0:
+        funcs_matching_ratio = 0.0
+        matching_ratio = 0.0
+    else:
+        funcs_matching_ratio = (len(matching_funcs) / len(all_funcs)) * 100
+        matching_ratio = (matching_size / total_size) * 100
+
+    content = {
+        "embeds": [{
+            "description": f"**{len(matching_funcs)}** matched functions / **{len(all_funcs)}** total (**{funcs_matching_ratio:.2f}%**)\n**{matching_size}** matching bytes / **{total_size}** total (**{matching_ratio:.2f}%**)"
+        }]
+    }
+    requests.post(args.discord_url, json=content)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Reports progress for the project on Discord")
+    parser.add_argument("--discord_url", help="Discord webhook URL", required=True)
+    args = parser.parse_args()
+    main(args)
