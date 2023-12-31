e06cd7227df61826b07ae00c6ca946a2fa37269c
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-12-30 21:15:35 -0300

    gTeamShieldsMapDisplay

--- src/overlays/ovl_EBFBE0/ED6EC0.c
+++ src/overlays/ovl_EBFBE0/ED6EC0.c
@@ -671,9 +671,9 @@ void func_EBFBE0_8019F600(void) {
     D_80177C38[1] = 255;
     D_80177C38[2] = 255;
     D_80177C38[3] = 255;
-    D_80177C58[1] = 255;
-    D_80177C58[2] = 255;
-    D_80177C58[3] = 255;
+    gTeamShieldsMapDisplay[1] = 255;
+    gTeamShieldsMapDisplay[2] = 255;
+    gTeamShieldsMapDisplay[3] = 255;
 
     D_EBFBE0_801CD950 = -1;
 
@@ -1616,7 +1616,7 @@ void func_EBFBE0_801A4AE8(void) {
 
                 if (D_EBFBE0_801CEFDC == 0) {
                     for (i = 0; i < 6; i++) {
-                        D_80177C58[i] = gTeamShields[i];
+                        gTeamShieldsMapDisplay[i] = gTeamShields[i];
                     }
                     D_EBFBE0_801CEFC8 = 0;
                     D_EBFBE0_801CD948 = 0;
@@ -3634,7 +3634,7 @@ void func_EBFBE0_801AD11C(void) {
             D_EBFBE0_801CD94C = 0;
         } else {
             for (i = 0; i < 6; i++) {
-                D_80177C58[i] = gTeamShields[i];
+                gTeamShieldsMapDisplay[i] = gTeamShields[i];
             }
             func_EBFBE0_801A659C();
         }
