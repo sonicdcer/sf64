2365010ce98c782cbdc895963479ebf4f92e6d30
inspectredc <inspectredc@gmail.com>
2024-01-11 02:56:15 +0000

    Change formatting output

--- tools/float-convert.py
+++ tools/float-convert.py
@@ -10,25 +10,40 @@ def main(argv):
     out = ""
     toFile = False
     toPrint = True
+    labelArr = ""
+    count = 0
     for opt, arg in opts:
         if opt in ("-f", "--file"):
             f = open(arg, "r")
             for line in f:
                 if "dlabel " in line:
+                    if count != 0:
+                        out += "[" + str(count) + "] {\n    " + labelArr + "\n}\n"
+                        out += "\n"
+                        labelArr = ""
+                        count = 0
                     label = line.split("dlabel ", 1)[1]
-                    out += "\n"
+                    out += "f32 "
                     out += label.split("\n",1)[0]
-                    out += "\n"
+                    # out += "\n"
                 elif " .float " in line:
+                    if count != 0:
+                        labelArr += ", "
                     seg = line.split(" .float ", 1)[1]
-                    out += seg.split("\n",1)[0] + ".0f"
-                    out += "\n"
+                    labelArr += seg.split("\n",1)[0] + ".0f"
+                    count += 1
+                    # out += "\n"
                 elif " .word 0x" in line:
+                    if count != 0:
+                        labelArr += ", "
                     seg = line.split(" .word 0x", 1)[1]
                     if len(seg) == 9:
-                        out += str(struct.unpack('!f', bytes.fromhex(seg))[0]) + "f"
-                        out += "\n"
+                        labelArr += str(struct.unpack('!f', bytes.fromhex(seg))[0]) + "f"
+                        count += 1
+                        # out += "\n"
                 elif " .asciz \"" in line:
+                    if count != 0:
+                        labelArr += ", "
                     seg = line.split(" .asciz \"", 1)[1]
                     length = len(seg) - 1
                     if length <= 4:
@@ -39,14 +54,17 @@ def main(argv):
                             segStr += format(ord(letter), "x")
                         for a in range (4 - length + 1):
                             segStr += "00"
-                        out += str(struct.unpack('!f', bytes.fromhex(segStr))[0]) + "f"
-                        out += "\n"
+                        labelArr += str(struct.unpack('!f', bytes.fromhex(segStr))[0]) + "f"
+                        count += 1
+                        # out += "\n"
+                
         elif opt in ("-o", "--output"):
             outputfile = arg
             toFile = True
         elif opt in ("-p", "--print"):
-            if arg == "false":
-                toPrint = False
+            if arg == "true":
+                toPrint = True
+    out += "[" + str(count) + "] {\n    " + labelArr + "\n}"
     if toPrint:
         print(out)
     if toFile:
@@ -54,7 +72,6 @@ def main(argv):
         outFile.write(out)
         outFile.close()
                     
-            
     sys.exit()
 
 if __name__ == "__main__":
