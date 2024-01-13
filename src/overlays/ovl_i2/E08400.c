bfc9658a96fce56dbd877fad1b2164780b3c1c58
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-01-13 19:05:19 -0300

    func_E08400_80187520

--- src/overlays/ovl_i2/E08400.c
+++ src/overlays/ovl_i2/E08400.c
@@ -4,4 +4,5 @@ UNK_TYPE D_E08400_80195D70;
 
 s32 D_E08400_80195420 = 0;
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/E08400/func_E08400_80187520.s")
+void func_E08400_80187520(s32 arg0, void* arg1) {
+}
