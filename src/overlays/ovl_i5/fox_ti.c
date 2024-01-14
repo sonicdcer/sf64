6066c5946710600b47fba3a4498755dea12dc9c0
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-01-14 00:32:40 -0300

    ovl_i2

---
+++ src/overlays/ovl_i5/fox_ti.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i5/fox_ti/func_i5_80187530.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i5/fox_ti/func_i5_801875D0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i5/fox_ti/func_i5_8018769C.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i5/fox_ti/func_i5_80188108.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i5/fox_ti/func_i5_801882CC.s")
