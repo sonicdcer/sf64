c5ea6b6eb5ffeb08546fd77bb6c381d0613a8e46
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-01-14 00:15:17 -0300

    ovl_i4 bss split

---
+++ src/overlays/ovl_i2/fox_sx.c
@@ -0,0 +1,51 @@
+#include "global.h"
+
+UNK_TYPE D_i2_80195D80;
+f32 D_i2_80195D88[3];
+f32 D_i2_80195D98[3];
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018F030.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018F124.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018F1DC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018F330.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018F884.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018FA04.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018FBBC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018FE38.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018FF20.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018FF40.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018FF84.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_8018FFDC.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_80190020.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_80190078.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_80192AF0.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_80193208.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_80193434.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_80193800.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_801938D8.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_80193A30.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_801944D4.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/func_i2_80194728.s")
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_sx/D_i2_80195A50.s")
