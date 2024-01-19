71e3a0a4f90edb0fe17386159f8ca802421cd96d
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-01-19 15:48:09 -0300

    func_i2_801881A8

--- src/overlays/ovl_i2/fox_me.c
+++ src/overlays/ovl_i2/fox_me.c
@@ -223,9 +223,23 @@ void func_i2_80187FF8(Effect* effect, f32 x, f32 y, f32 z) {
     Object_SetInfo(&effect->info, effect->obj.id);
 }
 
+// find out prototype first
 #pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_me/func_i2_80188088.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_me/func_i2_801881A8.s")
+void func_i2_801881A8(Effect* effect, f32 arg1, f32 arg2, f32 arg3, f32 arg4, s32 arg5) {
+    Effect_Initialize(effect);
+    effect->obj.status = 1;
+    effect->obj.id = OBJ_EFFECT_370;
+
+    effect->obj.pos.x = arg1;
+    effect->obj.pos.y = arg2;
+    effect->obj.pos.z = arg3;
+
+    effect->obj.rot.z = arg4;
+
+    effect->unk_4E = arg5;
+    Object_SetInfo(&effect->info, effect->obj.id);
+}
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i2/fox_me/func_i2_80188228.s")
 
