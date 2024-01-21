8311e7c622b77cf791700086cc6af446d48bb15b
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-01-21 17:53:12 -0300

    func_i4_80199900

--- src/overlays/ovl_i4/fox_sz.c
+++ src/overlays/ovl_i4/fox_sz.c
@@ -1,6 +1,42 @@
-#include "common.h"
+#include "global.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i4/fox_sz/func_i4_80199900.s")
+extern s32 D_i4_801A0560;
+
+void func_i4_80199900(Actor* arg0, s32 arg1) {
+    s32 i;
+
+    D_Timer_80161A60 = 8;
+
+    Object_Kill(&arg0->obj, arg0->sfxPos);
+    func_8007A6F0(&arg0->obj.pos, 0x2903A060);
+
+    for (i = 0; i < 20; i++) {
+        func_800794CC(arg0->obj.pos.x, arg0->obj.pos.y, arg0->obj.pos.z, 1.0f);
+        func_80079618(arg0->obj.pos.x, arg0->obj.pos.y, arg0->obj.pos.z, 1.0f);
+    }
+
+    func_8007BFFC(arg0->obj.pos.x, arg0->obj.pos.y, arg0->obj.pos.z, 0.0f, 0.0f, 0.0f, 20.0f, 0x1E);
+    func_8007B344(arg0->obj.pos.x, arg0->obj.pos.y, arg0->obj.pos.z, 20.0f, 5);
+
+    if (arg1 != 0) {
+        D_i4_801A0560++;
+        if ((D_i4_801A0560 >= 6) &&
+            ((gPlayer->state_1C8 == PLAYERSTATE_1C8_3) || (gPlayer->state_1C8 == PLAYERSTATE_1C8_5))) {
+            gCsFrameCount = 0;
+            gPlayer->state_1C8 = PLAYERSTATE_1C8_7;
+            gPlayer->unk_1D0 = 0x3E8;
+            gActors->unk_0B8 = 6;
+            gPlayer->timer_1F8 = 0x1E;
+            Audio_PlaySfx(0x11030010U, gActors->sfxPos, 0U, &D_800C5D34, &D_800C5D34, &D_800C5D3C);
+            Audio_PlaySfx(0x31024059U, gActors->sfxPos, 0U, &D_800C5D34, &D_800C5D34, &D_800C5D3C);
+            func_800182F4(0x100100FF);
+            func_800182F4(0x110100FF);
+        }
+        if ((D_i4_801A0560 == 3) && (D_800D3180[8] != 0)) {
+            D_800C9B4C = D_8015F928 + 110;
+        }
+    }
+}
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i4/fox_sz/func_i4_80199B18.s")
 
