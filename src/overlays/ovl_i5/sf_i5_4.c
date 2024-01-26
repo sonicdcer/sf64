aad94d466d29126e842320f760392a40985e3ab7
Ryan Myers <ryan.p.myers@gmail.com>
2024-01-25 20:33:05 -0500

    Match func_i5_801B5110

--- src/overlays/ovl_i5/sf_i5_4.c
+++ src/overlays/ovl_i5/sf_i5_4.c
@@ -1,5 +1,7 @@
 #include "global.h"
 
+void func_i5_801B5244(s32 arg0, s32 arg1);
+
 typedef struct {
     /* 0x00 */ s32 unk_00;
     /* 0x04 */ s32 unk_04;
@@ -11,9 +13,43 @@ typedef struct {
     /* 0x1C */ s16 unk_1C;
     /* 0x20 */ f32 unk_20;
 } UnkStruct_801C62E8; // size = 0x24
-extern UnkStruct_801C62E8 D_i5_801C62E8[20];
-extern f32 D_i5_801BE740;
+
+typedef struct {
+    /* 0x00 */ f32 unk_00;
+    /* 0x04 */ f32 unk_04;
+    /* 0x08 */ f32 unk_08;
+} UnkStruct_801C62D8; // size = unknown
+
 extern u8 D_i5_801BA970[12];
+extern f32 D_i5_801BE740;
+extern f32 D_i5_801BE744;
+extern s32 D_i5_801C5C04;
+extern s32 D_i5_801C5C08;
+extern s32 D_i5_801C5C0C;
+extern f32 D_i5_801C5C10;
+extern s32 D_i5_801C5C14;
+extern UnkStruct_801C62E8 D_i5_801C62E8[20];
+// extern UnkStruct_801C62E8 D_i5_801C1D48[49]; // Really unsure about this struct
+extern AnimationHeader D_8000708;
+extern AnimationHeader D_8000D80;
+extern AnimationHeader D_A0002BC;
+extern AnimationHeader D_A00047C;
+extern AnimationHeader D_A000858;
+extern AnimationHeader D_A000934;
+extern AnimationHeader D_A000D50;
+extern u8 D_i5_801B8C0C[24];
+extern Effect* D_i5_801BBEF0; // Not sure about struct type
+extern Player* D_i5_801BBEF4; // Not sure about struct type
+extern Vec3f D_i5_801BC978;
+extern Vec3f D_i5_801BC9D8;
+extern Vec3f D_i5_801BCA38;
+extern Vec3f D_i5_801BCDC8;
+extern Vec3f D_i5_801BCE28;
+extern Vec3f D_i5_801BCE88;
+extern s32 D_i5_801C1D48[28][16];
+extern f32 D_i5_801C24B8[28]; // size: 0x40
+extern f32 D_i5_801C2448[28]; // size: 0x40
+extern UnkStruct_801C62D8 D_i5_801C62D8;
 
 bool func_i5_801B49D0(Actor* actor) {
     UnkStruct_801C62E8* var_v0;
@@ -107,8 +143,8 @@ void func_i5_801B4AA8(s32* arg0, s32* arg1) {
                     for (j = 0; j < 16; j++, var_s1++) {
                         temp_fs0 = (j * 220.0f * D_i5_801BE740) - 1760.0f - unk_08;
                         if (fabsf(temp_fs0) <= unk_10) {
-                            temp_fs1 = __sinf((var_s2->unk_20 / unk_18) * M_PI);
-                            var_fv0 = __cosf((temp_fs0 / unk_10) * (M_PI / 2.0f));
+                            temp_fs1 = __sinf((var_s2->unk_20 / unk_18) * (M_DTOR * 180.0f));
+                            var_fv0 = __cosf((temp_fs0 / unk_10) * (M_DTOR * 90.0f));
                             *var_s1 = *var_s1 + (var_fv0 * unk_14 * temp_fs1);
                         }
                     }
@@ -144,10 +180,11 @@ void func_i5_801B4AA8(s32* arg0, s32* arg1) {
                     break;
                 case 3:
                     for (j = 0; j < 16; j++) {
-                        temp_fs0 = (__sinf(((var_s2->unk_20 * 8.0f) / var_s2->unk_18) * 2 * M_PI) * 500.0f) +
-                                   (((j * 220.0f * D_i5_801BE740) - 1760.0f) - var_s2->unk_08);
+                        temp_fs0 =
+                            (__sinf(((var_s2->unk_20 * 8.0f) / var_s2->unk_18) * 2 * (M_DTOR * 180.0f)) * 500.0f) +
+                            (((j * 220.0f * D_i5_801BE740) - 1760.0f) - var_s2->unk_08);
                         if (fabsf(temp_fs0) <= var_s2->unk_10) {
-                            temp_fa0 = __cosf((temp_fs0 / var_s2->unk_10) * (M_PI / 2.0f));
+                            temp_fa0 = __cosf((temp_fs0 / var_s2->unk_10) * (M_DTOR * 90.0f));
                             if (temp_fa0 >= 0.7) {
                                 temp_fa0 = 0.7f;
                             }
@@ -191,7 +228,37 @@ void func_i5_801B4AA8(s32* arg0, s32* arg1) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i5/sf_i5_4/func_i5_801B5110.s")
+void func_i5_801B5110(f32 arg0, f32 arg1, f32 arg2) {
+    UnkStruct_801C62E8* ptr;
+    s32 i;
+    s32 j;
+    s32 h;
+    s32 k;
+
+    D_i5_801C5C04 = 0;
+    D_i5_801C5C08 = 0;
+    D_i5_801C5C0C = 0;
+    D_i5_801C5C10 = 0.0f;
+    D_i5_801BE740 = D_i5_801BE744 = 1.0f;
+    D_i5_801C62D8.unk_00 = arg0;
+    D_i5_801C62D8.unk_04 = arg1;
+    D_i5_801C62D8.unk_08 = 200.0f; // Probably used to be arg2
+    D_i5_801C5C14 = 1;
+    for (i = 0, ptr = D_i5_801C62E8; i < ARRAY_COUNT(D_i5_801C62E8); i++, ptr++) {
+        func_i5_801B4A54(ptr);
+    }
+    for (i = 0; i < ARRAY_COUNT(D_i5_801C1D48); i++) {
+        // clang-format off
+        for (k = 0; k < ARRAY_COUNT(D_i5_801C1D48[0]); k += 4) { \
+            for (h = 0; h < 4; h++) {
+                D_i5_801C1D48[i][k + h] = 0;
+            }
+        }
+        // clang-format on
+        D_i5_801C2448[i] = D_i5_801C24B8[i] = 1.0f;
+    }
+    func_i5_801B5244(D_i5_801C5C04, D_i5_801C5C08);
+}
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i5/sf_i5_4/func_i5_801B5244.s")
 
