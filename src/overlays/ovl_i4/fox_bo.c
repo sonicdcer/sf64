7f2a8f47e0d483fa6a4766bd7fb407637bce5643
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-01-28 20:46:47 -0300

    func_i4_8018BD60

--- src/overlays/ovl_i4/fox_bo.c
+++ src/overlays/ovl_i4/fox_bo.c
@@ -1,6 +1,45 @@
-#include "common.h"
+#include "global.h"
 
+void func_8002FC00(Actor *);
+
+#ifdef IMPORT_DATA
+void func_i4_8018BD60(Actor* actor) {
+    s32 i;
+    Actor* actorPtr;
+    f32 D_i4_8019EEC4[4] = { 8000.0f, -8000.0f, 8000.0f, -8000.0f };
+    f32 D_i4_8019EED4[4] = { 8000.0f, 8000.0f, -8000.0f, -8000.0f };
+
+    if (D_8015F924 == 0) {
+        actor->unk_04E++;
+        if (actor->unk_04E >= 4) {
+            actor->unk_04E = 0;
+        }
+        for (actorPtr = gActors + 10, i = 10; i < 16; i++, actorPtr++) {
+            if (actorPtr->obj.status == 0) {
+                Actor_Initialize(actorPtr);
+                actorPtr->obj.status = 2;
+                actorPtr->obj.id = OBJ_ACTOR_197;
+                actorPtr->obj.pos.x = D_i4_8019EEC4[actor->unk_04E];
+                actorPtr->obj.pos.y = 1000.0f;
+                actorPtr->obj.pos.z = D_i4_8019EED4[actor->unk_04E];
+                actorPtr->state = 1;
+                actorPtr->unk_0E6 = -1;
+                actorPtr->health = 0x18;
+                actorPtr->iwork[11] = 1;
+                actorPtr->unk_044 = 2;
+                actorPtr->unk_0E4 = i;
+                Object_SetInfo(&actorPtr->info, actorPtr->obj.id);
+                Audio_PlaySfx(0x3100000CU, actorPtr->sfxPos, 4U, &D_800C5D34, &D_800C5D34, &D_800C5D3C);
+                break;
+            }
+        }
+    }
+    func_8002F69C(actor);
+    func_8002FC00(actor);
+}
+#else
 #pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i4/fox_bo/func_i4_8018BD60.s")
+#endif
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i4/fox_bo/func_i4_8018BEF8.s")
 
