a9caa3d48d9e096a3e4a96e5b16a5f0ae81807b2
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-02-06 20:13:26 -0300

    fox_enmy symbols

---
+++ include/fox_enmy_assets.h
@@ -0,0 +1,27 @@
+#ifndef FOX_ENMY_ASSETS_H
+#define FOX_ENMY_ASSETS_H
+
+#include "global.h"
+
+extern ObjectInit D_060371A4[];
+extern ObjectInit D_06026CC4[];
+extern ObjectInit D_0602A164[];
+extern ObjectInit D_06023F64[];
+extern ObjectInit D_060287A4[];
+extern ObjectInit D_0602E4F4[];
+extern ObjectInit D_06007E74[];
+extern ObjectInit D_0601F234[];
+extern ObjectInit D_06026714[];
+extern ObjectInit D_0C035154[];
+extern ObjectInit D_06006AA4[];
+extern ObjectInit D_06031000[];
+extern ObjectInit D_06006C60[];
+extern ObjectInit D_0602E5C8[];
+extern ObjectInit D_0600EAD4[];
+extern ObjectInit D_06011044[];
+extern ObjectInit D_0600FF74[];
+extern ObjectInit D_06006EB4[];
+extern ObjectInit D_06014D94[];
+extern ObjectInit D_0302DE3C[];
+
+#endif
