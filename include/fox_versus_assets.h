731f363dfc11d603f814f67a8ac5711688b91d8c
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-02-06 21:23:57 -0300

    fox_versus symbols

---
+++ include/fox_versus_assets.h
@@ -0,0 +1,37 @@
+#ifndef FOX_VERSUS_ASSETS_H
+#define FOX_VERSUS_ASSETS_H
+
+#include "global.h"
+
+extern u8 D_30087A0[];
+extern u8 D_3008EC0[];
+extern u8 D_3009990[];
+extern u8 D_3006040[];
+extern u8 D_30076C0[];
+extern u8 D_300B3F0[];
+extern u8 D_3004F60[];
+
+extern u16 D_3008DE0[];
+extern u16 D_30098C0[];
+extern u16 D_300A390[];
+extern u16 D_3006A68[];
+extern u16 D_3008598[];
+extern u16 D_300C458[];
+extern u16 D_3005E38[];
+
+extern u8 D_5000000[];
+extern u8 D_5000080[];
+extern u8 D_5000100[];
+extern u8 D_5000180[];
+extern u8 D_5000200[];
+extern u8 D_5000280[];
+extern u8 D_5000300[];
+extern u8 D_5000280[];
+extern u8 D_5000380[];
+extern u8 D_5000400[];
+extern u8 D_5000480[];
+extern u8 D_500A5F0[];
+extern u8 D_500A6E0[];
+extern u8 D_500A7D0[];
+
+#endif
