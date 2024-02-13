0ce4bf15150efb2159029ffa91eeece863b8944f
KiritoDv <kiritodev01@gmail.com>
2024-02-13 16:50:42 -0600

    Added a separation to libultra macros

--- include/macros.h
+++ include/macros.h
@@ -42,7 +42,6 @@
 #endif
 
 #define ALIGNED(x) __attribute__((aligned(x)))
-#define ARRLEN(x) ((s32)(sizeof(x) / sizeof(x[0])))
 #define STUBBED_PRINTF(x) ((void)(x))
 #define UNUSED __attribute__((unused))
 
