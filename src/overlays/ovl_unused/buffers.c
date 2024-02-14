5155c6211eb0253b33d0cb806214a45e9b132dfe
KiritoDv <kiritodev01@gmail.com>
2024-02-13 19:30:26 -0600

    Removed sizes and moved buffers to a new overlays segment

--- src/overlays/ovl_unused/buffers.c
+++ src/overlays/ovl_unused/buffers.c
@@ -4,9 +4,7 @@ u64 gDramStack[SP_DRAM_STACK_SIZE64];
 u8 gOSYieldData[OS_YIELD_DATA_SIZE];
 FrameBuffer gZBuffer; // z buffer
 u8 gTaskOutputBuffer[0x30000];
-u8 gTaskOutputBufferEnd;
 u8 gAudioDataBuffer[0xB0000];
 u16 gTextureRenderBuffer[0x3C40];
 u16 gFillBuffer[3 * SCREEN_WIDTH];
-u8 D_8038F300[0x500]; // Wrong
 FrameBuffer gFrameBuffers[3];
