0ff505b34cb9c7ea6e7792e46b9781556106fdc0
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-02-14 11:29:06 -0300

    func_8000BFD8

--- src/main/sys_audio_C870.c
+++ src/main/sys_audio_C870.c
@@ -25,7 +25,10 @@ void func_8000BF6C(s32* arg0, s32 arg1, s32 arg2) {
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/main/sys_audio_C870/func_8000BFA8.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/main/sys_audio_C870/func_8000BFD8.s")
+void func_8000BFD8(s32* arg0) {
+    arg0[3] = 0;
+    arg0[1] = arg0[0];
+}
 
 void func_8000BFE8(s32 arg0) {
     func_8000BF6C(&D_8014C1D0, gAudioDataBuffer, arg0);
