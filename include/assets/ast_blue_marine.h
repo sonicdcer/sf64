cb164c71fa32e05c62cacc9771694524433d8109
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-02-19 19:33:00 -0300

    progress so far

--- include/assets/ast_blue_marine.h
+++ include/assets/ast_blue_marine.h
@@ -5,4 +5,53 @@
 #include "sf64object.h"
 #include "structs.h"
 
+extern u8 D_3000000[];
+extern u8 D_3000080[];
+extern u8 D_3000090[];
+extern u8 D_3000120[];
+extern Gfx D_3000130[];
+extern Vtx ast_blue_marine_seg3_vtx_00000228[];
+extern Vtx ast_blue_marine_seg3_vtx_00000418[];
+extern Gfx D_3000470[];
+extern Vtx ast_blue_marine_seg3_vtx_00000528[];
+extern u8 D_3000600[];
+extern u8 D_3000640[];
+extern Gfx D_3000AF0[];
+extern Vtx ast_blue_marine_seg3_vtx_00000B60[];
+extern Gfx D_3000C70[];
+extern Vtx ast_blue_marine_seg3_vtx_00000FD0[];
+extern Vtx ast_blue_marine_seg3_vtx_000011D0[];
+extern Vtx ast_blue_marine_seg3_vtx_000013C0[];
+extern Vtx ast_blue_marine_seg3_vtx_00001400[];
+extern Vtx ast_blue_marine_seg3_vtx_000015D0[];
+extern Vtx ast_blue_marine_seg3_vtx_00001700[];
+extern Vtx ast_blue_marine_seg3_vtx_00001760[];
+extern Vtx ast_blue_marine_seg3_vtx_00001790[];
+extern Vtx ast_blue_marine_seg3_vtx_00001840[];
+extern u8 D_3001900[];
+extern u8 D_3001980[];
+extern u8 D_3002180[];
+extern u8 D_3002980[];
+extern u8 D_3002E80[];
+extern u8 D_3003180[];
+extern u8 D_3003980[];
+extern u8 D_3004180[];
+extern u8 D_3004980[];
+extern Gfx D_3005AB0[];
+extern Vtx ast_blue_marine_seg3_vtx_00005EB8[];
+extern Vtx ast_blue_marine_seg3_vtx_00006098[];
+extern Vtx ast_blue_marine_seg3_vtx_00006158[];
+extern Vtx ast_blue_marine_seg3_vtx_000061B8[];
+extern Vtx ast_blue_marine_seg3_vtx_00006218[];
+extern u8 D_30062E8[];
+extern Gfx D_3006AF0[];
+extern Vtx ast_blue_marine_seg3_vtx_00006B60[];
+extern Gfx D_3006C70[];
+extern Vtx ast_blue_marine_seg3_vtx_00006CF0[];
+extern Gfx D_3006DE0[];
+extern Vtx ast_blue_marine_seg3_vtx_00006F20[];
+extern Vtx ast_blue_marine_seg3_vtx_00007090[];
+extern Vtx ast_blue_marine_seg3_vtx_000071D0[];
+extern Vtx ast_blue_marine_seg3_vtx_00007290[];
+
 #endif
