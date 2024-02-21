6ae8609759582fbf14c82a944c663768749772bd
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-02-21 20:05:31 -0300

    disable AA

--- src/libultra/vimodes/vimodentsclan1.c
+++ src/libultra/vimodes/vimodentsclan1.c
@@ -6,7 +6,7 @@ OSViMode osViModeNtscLan1 = {
     OS_VI_NTSC_LAN1, // type
     {
         // comRegs
-        VI_CTRL_TYPE_16 | VI_CTRL_GAMMA_DITHER_ON | VI_CTRL_GAMMA_ON | VI_CTRL_DIVOT_ON | VI_CTRL_ANTIALIAS_MODE_1 |
+        VI_CTRL_TYPE_16 | VI_CTRL_GAMMA_DITHER_ON | VI_CTRL_GAMMA_ON | VI_CTRL_DIVOT_ON | VI_CTRL_ANTIALIAS_MODE_2 |
             VI_CTRL_PIXEL_ADV_3, // ctrl
         WIDTH(320),              // width
         BURST(57, 34, 5, 62),    // burst
