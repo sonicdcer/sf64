192402780a105ac06dbc056df26ed21d1a4e9f09
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-02-21 20:07:56 -0300

    enable level select & jukebox

--- include/mods.h
+++ include/mods.h
@@ -6,14 +6,14 @@
  * Press left or right on the D-PAD to choose which level to play,
  * useful for debugging and speedrunning training.
 */
-#define MODS_LEVEL_SELECT 0
+#define MODS_LEVEL_SELECT 1
 
 
 /**
  * Sound Effects Jukebox:
  * Ability to play sound effects inside the expert sound menu
 */
-#define MODS_SFX_JUKEBOX 0
+#define MODS_SFX_JUKEBOX 1
 
 /* ************************* */
 
