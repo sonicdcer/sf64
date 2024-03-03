5dd5534f952480d10633ec55960302550eb82ba5
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-03-02 22:12:21 -0300

    MATCHPOINT!

--- include/assets/ast_bg_space.h
+++ include/assets/ast_bg_space.h
@@ -5,4 +5,47 @@
 #include "sf64object.h"
 #include "structs.h"
 
+extern Gfx D_2000000[];
+extern Vtx ast_bg_space_seg2_vtx_00000058[];
+extern u16 D_2000088[];
+extern Gfx D_2000890[];
+extern Vtx ast_bg_space_seg2_vtx_000008E8[];
+extern u16 D_2000918[];
+extern Gfx D_2001120[];
+extern Vtx ast_bg_space_seg2_vtx_00001178[];
+extern u16 D_20011A8[];
+extern Gfx D_20019B0[];
+extern Vtx ast_bg_space_seg2_vtx_00001A08[];
+extern u16 D_2001A38[];
+extern Gfx D_2002240[];
+extern Vtx ast_bg_space_seg2_vtx_00002298[];
+extern u16 D_20022C8[];
+extern Gfx D_2002AD0[];
+extern Vtx ast_bg_space_seg2_vtx_00002B28[];
+extern u16 D_2002B58[];
+extern Gfx D_2003360[];
+extern Vtx ast_bg_space_seg2_vtx_000033B8[];
+extern u16 D_20033E8[];
+extern Gfx D_2003BF0[];
+extern Vtx ast_bg_space_seg2_vtx_00003C48[];
+extern u16 D_2003C78[];
+extern Gfx D_2004480[];
+extern Vtx ast_bg_space_seg2_vtx_000044D8[];
+extern u16 D_2004508[];
+extern Gfx D_2004D10[];
+extern Vtx ast_bg_space_seg2_vtx_00004D68[];
+extern u16 D_2004D98[];
+extern Gfx D_20055A0[];
+extern Vtx ast_bg_space_seg2_vtx_000055F8[];
+extern u16 D_2005628[];
+extern Gfx D_2005E30[];
+extern Vtx ast_bg_space_seg2_vtx_00005E88[];
+extern u16 D_2005EB8[];
+extern Gfx D_20066C0[];
+extern Vtx ast_bg_space_seg2_vtx_00006718[];
+extern u16 D_2006748[];
+extern Gfx D_2006F50[];
+extern Vtx ast_bg_space_seg2_vtx_00006FA8[];
+extern u16 D_2006FD8[];
+
 #endif
