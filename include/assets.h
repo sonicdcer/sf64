e03a0bee52f20fb6098702c37c2bfdb21e5f75d4
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-03-06 18:50:24 -0300

    clean up unnecessary externs

--- include/assets.h
+++ include/assets.h
@@ -1026,24 +1026,6 @@ extern f32 D_C038DC0[];
 extern f32 D_C038F24[];
 extern Gfx D_C039208[];
 
-// ast_allies
-extern Gfx D_D000000[];
-extern u16 D_D000170[];
-extern u16 D_D001090[];
-extern u16 D_D001FB0[];
-extern u16 D_D002ED0[];
-extern u16 D_D003DF0[];
-extern u16 D_D004D10[];
-extern u16 D_D005C30[];
-extern u16 D_D006B50[];
-extern u16 D_D007A70[];
-extern u16 D_D008990[];
-extern Gfx D_D0098B0[];
-extern Gfx D_D009A40[];
-extern Gfx D_D00B5C0[];
-extern Gfx D_D00B720[];
-extern Gfx D_D00B880[];
-
 // ast_great_fox
 extern Gfx D_E000000[];
 extern Gfx D_E003AB0[];
