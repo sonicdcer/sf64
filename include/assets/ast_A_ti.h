03d1926507f87eec94f7bc012d719fa89a9b94d5
petrie911 <petrie911@yahoo.com>
2024-03-11 19:54:21 -0500

    skellingtons

--- include/assets/ast_A_ti.h
+++ include/assets/ast_A_ti.h
@@ -5,4 +5,31 @@
 #include "sf64object.h"
 #include "structs.h"
 
+extern Gfx D_A000000[];
+extern Vtx ast_A_ti_seg10_vtx_98[];
+extern u8 D_A0000C8[];
+extern u16 D_A0001C8[];
+extern Animation D_A0002BC;
+extern Animation D_A00047C;
+extern Limb* D_A000568[];
+extern Animation D_A000858;
+extern Animation D_A000934;
+extern Animation D_A000D50;
+extern Limb* D_A000EDC[];
+extern Limb* D_A001A70[];
+extern Gfx D_A001BE0[];
+extern Vtx ast_A_ti_seg10_vtx_1D08[];
+extern Vtx ast_A_ti_seg10_vtx_1D58[];
+extern Gfx D_A001DB0[];
+extern Vtx ast_A_ti_seg10_vtx_1E50[];
+extern Gfx D_A001EC0[];
+extern Vtx ast_A_ti_seg10_vtx_1F60[];
+extern Gfx D_A001FA0[];
+extern Vtx ast_A_ti_seg10_vtx_20C8[];
+extern Vtx ast_A_ti_seg10_vtx_2118[];
+extern Gfx D_A002170[];
+extern Vtx ast_A_ti_seg10_vtx_2298[];
+extern Vtx ast_A_ti_seg10_vtx_2308[];
+extern Animation D_A009990;
+
 #endif
