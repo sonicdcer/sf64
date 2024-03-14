79ed0a9056bfbc604b2d8fd85da635011cd062d4
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-03-14 00:11:06 -0300

    Matchpoint (2 blobs)

--- include/assets/ast_training.h
+++ include/assets/ast_training.h
@@ -4,5 +4,36 @@
 #include "gfx.h"
 #include "sf64object.h"
 #include "structs.h"
+#include "sf64level.h"
+
+extern u16 D_6000900[];
+extern u16 D_6001820[];
+extern Gfx D_6002740[];
+extern Vtx ast_training_seg6_vtx_27F0[];
+extern Vtx ast_training_seg6_vtx_2870[];
+extern u16 D_60028F0[];
+extern Gfx D_60030F0[];
+extern Vtx ast_training_seg6_vtx_3220[];
+extern Vtx ast_training_seg6_vtx_32A0[];
+extern Vtx ast_training_seg6_vtx_32E0[];
+extern Vtx ast_training_seg6_vtx_3320[];
+extern u16 D_6003360[];
+extern u16 D_6003560[];
+extern Gfx D_6003760[];
+extern Vtx ast_training_seg6_vtx_37F8[];
+extern Vtx ast_training_seg6_vtx_3838[];
+extern u16 D_6003878[];
+extern u16 D_6004878[];
+extern Gfx D_6005880[];
+extern Vtx ast_training_seg6_vtx_58D8[];
+extern u16 D_6005938[];
+extern Gfx D_6006140[];
+extern Vtx ast_training_seg6_vtx_62D8[];
+extern Vtx ast_training_seg6_vtx_64D8[];
+extern Vtx ast_training_seg6_vtx_6628[];
+extern Vtx ast_training_seg6_vtx_6828[];
+extern u16 D_6006958[];
+extern u16 D_60069D8[];
+extern EnvSettings D_6006A60;
 
 #endif
