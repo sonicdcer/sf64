1e4038127247af1e89ef609131527ed5dbfbe7bb
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-03-17 19:44:40 -0300

    progress

--- include/assets/ast_menu.h
+++ include/assets/ast_menu.h
@@ -5,4 +5,26 @@
 #include "sf64object.h"
 #include "structs.h"
 
+extern u8 D_MENU_6000000[];
+extern u8 D_MENU_6000840[];
+extern u8 D_MENU_6001080[];
+extern u8 D_MENU_60018C0[];
+extern u8 D_MENU_6001C80[];
+extern u8 D_MENU_6002160[];
+extern u8 D_MENU_60030D0[];
+extern u8 D_MENU_6003B50[];
+extern u8 D_MENU_6004AC0[];
+extern u8 D_MENU_6005740[];
+extern u8 D_MENU_6006320[];
+extern u8 D_MENU_6006E80[];
+extern u8 D_MENU_6007B90[];
+extern u8 D_MENU_6008990[];
+extern u8 D_MENU_6009AD0[];
+extern u8 D_MENU_600AD30[];
+extern u8 D_MENU_600B9B0[];
+extern u8 D_MENU_600CC10[];
+extern u8 D_MENU_600D590[];
+extern u8 D_MENU_600E210[];
+extern u8 D_MENU_600F390[];
+
 #endif
