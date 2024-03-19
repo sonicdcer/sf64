e18d415192d49cdc4dd4d1cb00330eff32757e00
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-03-19 00:32:44 -0300

    progress

--- include/assets/ast_menu.h
+++ include/assets/ast_menu.h
@@ -41,5 +41,26 @@ extern u8 D_MENU_6018280[];
 extern u8 D_MENU_6019030[];
 extern u8 D_MENU_60190B0[];
 extern u8 D_MENU_601B4B0[];
+extern u8 D_MENU_1B8B0[];
+extern Gfx D_MENU_601C0D0[];
+extern Vtx ast_menu_seg6_vtx_1C120[];
+extern u16 D_MENU_601C160[];
+extern Gfx D_MENU_601C960[];
+extern Vtx ast_menu_seg6_vtx_1C9B0[];
+extern u16 D_MENU_601C9F0[];
+extern Gfx D_MENU_601D1F0[];
+extern Vtx ast_menu_seg6_vtx_1D290[];
+extern u8 D_MENU_601D2F0[];
+extern u16 D_MENU_601DAF0[];
+extern Gfx D_MENU_601DCF0[];
+extern Vtx ast_menu_seg6_vtx_1DD98[];
+extern u8 D_MENU_601DDE8[];
+extern u16 D_MENU_601DE68[];
+extern Gfx D_MENU_601DE80[];
+extern Vtx ast_menu_seg6_vtx_1E098[];
+extern Vtx ast_menu_seg6_vtx_1E298[];
+extern Vtx ast_menu_seg6_vtx_1E498[];
+extern Vtx ast_menu_seg6_vtx_1E698[];
+extern Vtx ast_menu_seg6_vtx_1E7D8[];
 
 #endif
