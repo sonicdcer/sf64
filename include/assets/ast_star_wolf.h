c6c0ce1a225dc7d4c20787410657ae319efbceab
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-03-27 12:15:02 -0300

    MISSION ACCOMPLISHED

--- include/assets/ast_star_wolf.h
+++ include/assets/ast_star_wolf.h
@@ -4,26 +4,62 @@
 #include "gfx.h"
 #include "sf64object.h"
 #include "structs.h"
+#include "sf64level.h"
 
-extern u16 D_F000000[];
-extern u16 D_F000F20[];
-extern u16 D_F001E40[];
-extern u16 D_F002D60[];
-extern u16 D_F003C80[];
-extern u16 D_F004BA0[];
-extern u16 D_F005AC0[];
-extern u16 D_F0069E0[];
-extern u16 D_F007900[];
-extern u16 D_F008820[];
-extern u16 D_F009740[];
-extern u16 D_F00A660[];
-extern u16 D_F00B580[];
-extern u16 D_F00C4A0[];
-extern u16 D_F00D3C0[];
-extern u16 D_F00E2E0[];
-extern Gfx D_F00F200[];
-extern Gfx D_F0103D0[];
-extern Gfx D_F014180[];
-extern Gfx D_F014310[];
+extern u16 D_STAR_WOLF_F000000[];
+extern u16 D_STAR_WOLF_F000F20[];
+extern u16 D_STAR_WOLF_F001E40[];
+extern u16 D_STAR_WOLF_F002D60[];
+extern u16 D_STAR_WOLF_F003C80[];
+extern u16 D_STAR_WOLF_F004BA0[];
+extern u16 D_STAR_WOLF_F005AC0[];
+extern u16 D_STAR_WOLF_F0069E0[];
+extern u16 D_STAR_WOLF_F007900[];
+extern u16 D_STAR_WOLF_F008820[];
+extern u16 D_STAR_WOLF_F009740[];
+extern u16 D_STAR_WOLF_F00A660[];
+extern u16 D_STAR_WOLF_F00B580[];
+extern u16 D_STAR_WOLF_F00C4A0[];
+extern u16 D_STAR_WOLF_F00D3C0[];
+extern u16 D_STAR_WOLF_F00E2E0[];
+extern Gfx D_STAR_WOLF_F00F200[];
+extern Vtx ast_star_wolf_seg15_vtx_F590[];
+extern Vtx ast_star_wolf_seg15_vtx_F780[];
+extern Vtx ast_star_wolf_seg15_vtx_F810[];
+extern Vtx ast_star_wolf_seg15_vtx_F9F0[];
+extern Vtx ast_star_wolf_seg15_vtx_FB30[];
+extern Vtx ast_star_wolf_seg15_vtx_FC60[];
+extern Vtx ast_star_wolf_seg15_vtx_FCB0[];
+extern Vtx ast_star_wolf_seg15_vtx_FE90[];
+extern Vtx ast_star_wolf_seg15_vtx_FF20[];
+extern Vtx ast_star_wolf_seg15_vtx_10110[];
+extern u16 D_STAR_WOLF_F0101D0[];
+extern Gfx D_STAR_WOLF_F0103D0[];
+extern Vtx ast_star_wolf_seg15_vtx_107A0[];
+extern Vtx ast_star_wolf_seg15_vtx_10980[];
+extern Vtx ast_star_wolf_seg15_vtx_10A10[];
+extern Vtx ast_star_wolf_seg15_vtx_10AD0[];
+extern Vtx ast_star_wolf_seg15_vtx_10C90[];
+extern Vtx ast_star_wolf_seg15_vtx_10DE0[];
+extern Vtx ast_star_wolf_seg15_vtx_10FD0[];
+extern Vtx ast_star_wolf_seg15_vtx_11010[];
+extern Vtx ast_star_wolf_seg15_vtx_11070[];
+extern Vtx ast_star_wolf_seg15_vtx_11110[];
+extern Vtx ast_star_wolf_seg15_vtx_11300[];
+extern Vtx ast_star_wolf_seg15_vtx_11500[];
+extern u16 D_STAR_WOLF_F011680[];
+extern u16 D_STAR_WOLF_F011E80[];
+extern u16 D_STAR_WOLF_F011F00[];
+extern u16 D_STAR_WOLF_F012700[];
+extern u16 D_STAR_WOLF_F012F00[];
+extern u16 D_STAR_WOLF_F013100[];
+extern u16 D_STAR_WOLF_F013180[];
+extern u16 D_STAR_WOLF_F013980[];
+extern Gfx D_STAR_WOLF_F014180[];
+extern Vtx ast_star_wolf_seg15_vtx_141D8[];
+extern u8 D_STAR_WOLF_F014208[];
+extern Gfx D_STAR_WOLF_F014310[];
+extern Vtx ast_star_wolf_seg15_vtx_14368[];
+extern u8 D_STAR_WOLF_F014398[];
 
 #endif
