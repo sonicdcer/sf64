1ee676279710594486d656cc0af40e605b74a24b
Alejandro Asenjo Nitti <96613413+sonicdcer@users.noreply.github.com>
2024-04-15 20:15:45 -0300

    Initial GCC Support (#212)
    * first try
    
    * progress
    
    * progress
    
    * gcc compiling, rom not booting yet
    
    * format
    
    * restore word compiler
    
    * comment
    
    * fix
    
    * fix
    
    * fix addresses for new splat
    
    * progress
    
    * splat
    
    * we don't need these anymore
    
    * gcc build seems stable now

--- src/libultra/gu/sqrtf.c
+++ src/libultra/gu/sqrtf.c
@@ -1,6 +1,10 @@
 #include "PR/ultratypes.h"
 #include "libc/math.h"
 
+#ifdef COMPILER_GCC
+#include "src/libultra/gcc_fix/sqrtf.c.inc"
+#endif
+
 f32 guSqrtf(f32 value) {
     return sqrtf(value);
 }
