5321fb8b55e612b8cf117aa6725959ab0ecef05f
petrie911 <petrie911@yahoo.com>
2024-04-15 18:25:10 -0500

    format plz

--- src/audio/audio_load.c
+++ src/audio/audio_load.c
@@ -1276,9 +1276,9 @@ s32 AudioLoad_RelocateFontAndPreloadSamples(s32 fontId, u32 fontDataAddr, Sample
             break;
         }
         sample = gUsedSamples[i];
-        #ifdef AVOID_UB
+#ifdef AVOID_UB
         sampleRamAddr = NULL;
-        #endif
+#endif
         //! Bug: Those are assignments, not equality checks.
         switch (isAsync) {
             case 0:
