58250006269be48e3f626f628c325c2d371d4b94
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-12-26 02:51:51 -0300

    gotMedal should be s32

--- src/overlays/ovl_menu/fox_option.c
+++ src/overlays/ovl_menu/fox_option.c
@@ -2463,7 +2463,7 @@ void Option_RankingPlanetRoute_Draw(s32 rankIdx, f32 y, s32 routeMax) {
     static f32 xAdvance = 40.1f;
     s32 i;
     PlanetId planet;
-    bool gotMedal;
+    s32 gotMedal;
     bool drawPlanetMedal;
     f32 x;
     s32 pad[2];
